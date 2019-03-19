1abb04aae3e4116cb999e2ffe3a6c291cbdb3c1b
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-03-19 16:27:59 +0530

    Create array2.py

---
+++ array2.py
@@ -0,0 +1,17 @@
+def condition():
+	            n=int(input())
+            	k=int(input())
+          	l=[]
+            	(s1,s2)=([],[])
+                	for i in range(n):
+            		l.append(int(input()))
+            	s1=l[:n-1]
+                                		s2=l[n-1:n]
+                 	else:
+             		s1=l[:n//2]
+            s2=l[n//2:n]
+                 print(max(min(s1),min(s2)))
+                              try:
+                  condition()
+             except:
+                           print('invalid')
